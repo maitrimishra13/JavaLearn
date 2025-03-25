d96aa56957a2bf28667954f00fc41055bc30c1ad
maitrimishra13 <delhi.maitri@gmail.com>
2025-03-25 20:04:52 +0530

    functions in java are known as methods

---
+++ class1.java
@@ -0,0 +1,9 @@
+public class class1 {
+    public static void main(String[] args) {
+        sayhi();
+        System.out.println("Hello World");
+    }
+    public static void sayhi() {
+        System.out.println("hello everyone");
+    }
+}
---
+++ class2.java
@@ -0,0 +1,5 @@
+public class class2 {
+    public static void main(String[] args) {
+        class1.sayhi();
+    }
+}
